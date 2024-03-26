777adad7ea7db795505015a89106a728614bb7bb
tmenguy <thomas@wildmoka.com>
2024-03-27 00:10:26 +0100

    reset energy sensor everyday

--- custom_components/netatmo/data_handler.py
+++ custom_components/netatmo/data_handler.py
@@ -571,7 +571,7 @@ class NetatmoDataHandler:
         self.publisher[signal_name] = NetatmoPublisher(
             name=signal_name,
             interval=interval,
-            next_scan=time() + interval//8 + n*delta_scan, #at init time try to get some data
+            next_scan=time() + interval//2 + n*delta_scan, #at init time try to get some data
             target=target,
             subscriptions={update_callback},
             method=PUBLISHERS[publisher],
